0c131164f00e1fdf7a2b7411e91f1227fd397cfc
arogozin <aleksandr@rogozin.us>
2014-12-15 22:19:52 -0500

    All views + controllers

---
+++ AfterHours/EqViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EqViewController.swift
+//  AfterHours
+//
+//  Created by Aleksandr Rogozin on 12/15/14.
+//  Copyright (c) 2014 Aleksandr Rogozin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EqViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        //
+    }
+}
---
+++ AfterHours/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  AfterHours
+//
+//  Created by Aleksandr Rogozin on 12/15/14.
+//  Copyright (c) 2014 Aleksandr Rogozin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        //
+    }
+}
