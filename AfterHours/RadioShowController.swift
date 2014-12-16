ef6e8ed3c0ea64bd1eba65947e158be348fb4a8d
arogozin <arogozin@nyu.edu>
2014-12-16 10:50:11 -0500

    More changes

--- AfterHours/RadioShowController.swift
+++ AfterHours/RadioShowController.swift
@@ -10,8 +10,22 @@ import Foundation
 import UIKit
 
 class RadioShowController: UIViewController {
+    @IBAction func playlistPressed(sender: AnyObject) {
+    }
+    @IBOutlet weak var playButton: UIButton!
+    @IBAction func playButtonPressed(sender: AnyObject) {
+    }
 
+    @IBOutlet weak var radioshowLabel: UILabel!
+    @IBOutlet weak var djLabel: UILabel!
+    
     override func viewDidLoad() {
-        //
+        var firebase = Firebase(url: "https://ahfm.firebaseio.com/playlist")
+        firebase.observeEventType(.Value, withBlock: {
+            snapshot in
+            self.radioshowLabel.text = snapshot.value.objectForKey("title") as? String
+            self.djLabel.text = snapshot.value.objectForKey("dj") as? String
+        })
+
     }
 }
