f29dabe57051ca3395f52aac07c91061345df8b3
usrfrndly <jmh794@nyu.edu>
2014-12-16 01:01:01 -0500

    Fixed keyboard overlap with input text

--- AfterHours/ChatController.swift
+++ AfterHours/ChatController.swift
@@ -8,9 +8,11 @@
 
 import Foundation
 import UIKit
+import CoreImage
 
 class ChatController: UIViewController , UITableViewDelegate, UITableViewDataSource{
     
+    @IBOutlet var scrollView:UIScrollView!
     @IBOutlet weak var messagesTableView: UITableView!
     @IBOutlet weak var radioshowLabel: UILabel!
     @IBOutlet weak var djLabel: UILabel!
@@ -19,28 +21,18 @@ class ChatController: UIViewController , UITableViewDelegate, UITableViewDataSou
     var messages:Firebase!
     let cellIdentifier = "chatCell"
     var chatsArray:[(author:String,text:String)] = []
-
+    @IBOutlet var inputMessageField:UITextField!
+    @IBOutlet var messageSendButton: UIButton!
+    var activeField:UITextField?
+    
     override func viewDidLoad() {
-        
-        // Do any additional setup after loading the view, typically from a nib.
-        
-//        if self.player.isPlaying() {
-//            var newBackgroundImg = UIImage(named: "Play.png")
-//            self.playerControlButton.setBackgroundImage(newBackgroundImg, forState: .Normal )
-//        }
-//        else {
-//            var newBackgroundImg = UIImage(named: "Pause.png")
-//            self.playerControlButton.setBackgroundImage(newBackgroundImg, forState: .Normal )
-//        }
-
-        
         var firebase = Firebase(url: "https://ahfm.firebaseio.com/playlist")
         firebase.observeEventType(.Value, withBlock: {
             snapshot in
             self.radioshowLabel.text = snapshot.value.objectForKey("title") as? String
             self.djLabel.text = snapshot.value.objectForKey("dj") as? String
         })
-
+        
         messages = Firebase(url: "https://ahfm.firebaseio.com/messages")
         messages.observeEventType(.ChildAdded, withBlock: {
             (snapshot) in
@@ -48,19 +40,62 @@ class ChatController: UIViewController , UITableViewDelegate, UITableViewDataSou
             let text = snapshot.value["text"] as? String
             self.chatsArray.append(author:author!, text:text!)
             println(snapshot.value)
-
-        
         })
-
+        //Whether a user is currently typing in the field
+        activeField = inputMessageField
         
-//            self.messagesTableView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
-//            view.addSubview(self.messagesTableView)
+        //Looks for double taps to dismiss the keyboard
+        var tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
+        self.view.addGestureRecognizer(tap)
         super.viewDidLoad()
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        self.registerForKeyboardNotifications()
+    }
+    override func viewWillDisappear(animated: Bool) {
+        self.deregisterFromKeyboardNotifications()
+        super.viewWillDisappear(true)
+    }
+    
     override func viewDidAppear(animated: Bool) {
         self.messagesTableView.reloadData()
     }
+    
+    func keyboardWasShown(notification: NSNotification) {
+        var info: Dictionary = notification.userInfo!
+        var keyboardSize: CGSize = (info[UIKeyboardFrameBeginUserInfoKey]?.CGRectValue().size)!
+        var contentInsets = UIEdgeInsetsMake(0.0,0.0,keyboardSize.height,0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        
+        //Scroll up if text field is hidden by keyboard
+        var aRect:CGRect = self.view.frame
+        aRect.size.height -= keyboardSize.height
+        if (!CGRectContainsPoint(aRect, self.activeField!.frame.origin) ) {
+            self.scrollView.scrollRectToVisible(activeField!.frame, animated: true)
+        }
+    }
+    
+    
+    func keyboardWillBeHidden(notification: NSNotification) {
+        //self.scrollView.setContentOffset(CGPointZero, animated: true)
+        var contentInsets:UIEdgeInsets = UIEdgeInsetsZero
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    // track the activeField to see the current editing status
+    func textFieldDidBeginEditing(textField:UITextField){
+        activeField = textField;
+    }
+    
+    func textFieldDidEndEditing(textField:UITextField){
+        activeField = nil
+        self.scrollView.setContentOffset(CGPointMake(0, 0), animated:true);
+    }
+    
     @IBAction func playerControlPressed(sender: AnyObject) {
         if self.player.isPlaying(){
             println("\(reflect(self).summary).\(__FUNCTION__)(): Pause stream")
@@ -75,6 +110,7 @@ class ChatController: UIViewController , UITableViewDelegate, UITableViewDataSou
         }
     }
     
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
@@ -88,17 +124,41 @@ class ChatController: UIViewController , UITableViewDelegate, UITableViewDataSou
         if let cell = tableView.dequeueReusableCellWithIdentifier(self.cellIdentifier) as? UITableViewCell{
             cell.textLabel!.text = "\(self.chatsArray[indexPath.row].text)"
             return cell
-
+            
         }else{
-             newCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: self.cellIdentifier)
+            newCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: self.cellIdentifier)
+            
+            
+            var image = UIImage(named: "hat-guy.png")
+            newCell.imageView?.image = image
+            newCell.textLabel!.text = "\(self.chatsArray[indexPath.row].text)"
+            newCell.detailTextLabel?.text = "\(self.chatsArray[indexPath.row].author)"
+            
+            return newCell
+        }
+    }
+    //MARK: Keyboard Avoidance
+    func registerForKeyboardNotifications() -> Void {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardWillShowNotification, object: nil)
         
-
-        var image = UIImage(named: "hat-guy.png")
-        newCell.imageView?.image = image
-        newCell.textLabel!.text = "\(self.chatsArray[indexPath.row].text)"
-        newCell.detailTextLabel?.text = "\(self.chatsArray[indexPath.row].author)"
-
-        return newCell
     }
+    
+    func deregisterFromKeyboardNotifications() -> Void {
+        println("Deregistering!")
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardDidHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardWillHideNotification, object: nil)
+        
     }
+    
+    
+    //Calls this function when the tap is recognized.
+    func DismissKeyboard(){
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        inputMessageField.resignFirstResponder()
+    }
+    
+    
 }
+
+
